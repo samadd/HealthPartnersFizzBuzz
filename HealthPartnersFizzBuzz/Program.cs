64e1ee79f109c320f825ad23a8369f8ff271bc96
samadd <samueljaddison@gmail.com>
2025-10-02 14:35:07 +0100

    Use implemented classes to generate required output

--- HealthPartnersFizzBuzz/Program.cs
+++ HealthPartnersFizzBuzz/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections.Immutable;
+using HealthPartnersFizzBuzz;
 
-Console.WriteLine("Hello, World!");
+var inputs = Enumerable.Range(1, 100);
+
+var conditions = new FizzBuzzConditions();
+var fizzBuzzer = new FizzBuzzer()
+    .AddCondition(conditions.FizzBuzz)
+    .AddCondition(conditions.Fizz)
+    .AddCondition(conditions.Buzz)
+    .AddPresenter(new FizzBuzzTextPresenter(Console.WriteLine));
+
+inputs.ToImmutableList().ForEach(number =>
+{
+    var answer = fizzBuzzer.Answer(number);
+    fizzBuzzer.Present(answer);
+});
